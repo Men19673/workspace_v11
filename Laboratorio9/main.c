8c2bd40bd65bc8175a4a502348dc6454fb940c3d
Men19673 <men19673@uvg.edu.gt>
2021-11-04 01:27:20 -0600

    PostLab9

--- Laboratorio9/main.c
+++ Laboratorio9/main.c
@@ -18,21 +18,34 @@
 #define XTAL 16000000
 
 uint8_t Flag;
+uint8_t mplex;
 
 void delayMs(uint32_t);
 void semaforo(void);
-
+void Timer0IntHandler(void);
 void main(void)
-{
+{   //Reloj
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);  //40MHz con PLL
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);  // Se asigna reloj a puerto F
+    //PORTF
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3); // Establecer los LEDS portF
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-    SysCtlPeripheralEnable(TIMER0_BASE);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);   //Configurar el WeakPullup
+
+    //Activar interrupciones generales
+    IntMasterEnable();
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0));        //Esperar a que se inicialice
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);            //Configurar el timer para funcion periodica
+    TimerLoadSet(TIMER0_BASE, TIMER_A, (SysCtlClockGet()/2));   //Configurar el overload
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);            //Activar int del timer
+    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);   //Registro del interrupt
+    IntEnable(INT_TIMER0A);                                     //Habilitar Int
+    TimerEnable(TIMER0_BASE, TIMER_A);                          //Enable
+
 
     while(1){
-     semaforo();
 
 
     }
@@ -72,4 +85,16 @@ void delayMs(uint32_t ui32Ms) {
     // 0.001 second = 1 ms = SysCtlClockGet() / 3 / 1000
 
     SysCtlDelay(ui32Ms * (SysCtlClockGet() / 3 / 1000));
+}
+void Timer0IntHandler(void){
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    if(mplex == 1){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x04);
+        mplex = 0;
+    }
+    else{
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x00);
+        mplex = 1;
+     }
 }
